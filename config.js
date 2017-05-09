b2ae40f44aec52c60865122742e8ad01b3bb256c
Weje Emmanuel <e.weje@concept-nova.com>
2017-05-09 13:30:56 +0100

    Initial Commit

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
             host: 'mailtrap.io',
             port: 2525,
             auth: {
-                user: '6105**********',
-                pass: '9249**********'
+                user: '6105**********', //use yours
+                pass: '9249**********' //use yours
             }
         });
 
